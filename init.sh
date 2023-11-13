63df693e2778c25381fdb81c5695303de1c70ce9
Hasan <haisi@Hasans-MBP.yallo.box>
2023-11-13 21:40:17 +0100

    Improve init script

--- init.sh
+++ init.sh
@@ -1,3 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[i] Ask for sudo password"
+sudo -v
+
+case "$(uname -s)" in
+    Darwin)
+        # install Command Line Tools
+        if [[ ! -x /usr/bin/gcc ]]; then
+          echo "[i] Install macOS Command Line Tools"
+          xcode-select --install
+        fi
+
+        # install homwbrew
+        if [[ ! -x /usr/local/bin/brew ]]; then
+          echo "[i] Install Homebrew"
+          /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+
+        # install ansible
+        if [[ ! -x /usr/local/bin/ansible ]]; then
+            echo "[i] Install Ansible"
+            brew install ansible
+        fi
+esac
+
 # Run main playbook
 echo "[i] Run Playbook"
 ansible-playbook main.yml --ask-become-pass
