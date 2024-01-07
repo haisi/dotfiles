0012893feb43da3210084d1f44e6ab615dfb1d4b
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-01-07 18:08:32 +0100

    Remove useless file

--- start.sh
+++
@@ -1,7 +0,0 @@
-sudo xcode-select --install
-
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/haisi/.zprofile
-    eval "$(/usr/local/bin/brew shellenv)"
